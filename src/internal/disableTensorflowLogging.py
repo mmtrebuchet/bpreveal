b0d46fdd077973ec0dc2ddd7e4046f193ea1cf24
Charles McAnany <git@charlesmcanany.com>
2024-07-23 14:58:03 -0500

    Stashing 5.0.0 work to bug fix 4.3.0.

--- src/internal/disableTensorflowLogging.py
+++ src/internal/disableTensorflowLogging.py
@@ -15,3 +15,4 @@ import tensorflow as tf
 tf.get_logger().setLevel("ERROR")
 warnings.simplefilter("ignore")
 bpreveal.logUtils.debug("Tensorflow logging successfully disabled.")
+# Copyright 2022, 2023, 2024 Charles McAnany. This file is part of BPReveal. BPReveal is free software: You can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. BPReveal is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with BPReveal. If not, see <https://www.gnu.org/licenses/>.  # noqa
--- src/makePisaFigure.py
+++ src/makePisaFigure.py
@@ -92,3 +92,4 @@ if __name__ == "__main__":
     config = interpreter.evalFile(sys.argv[1])
     assert isinstance(config, dict)
     main(config)
+# Copyright 2022, 2023, 2024 Charles McAnany. This file is part of BPReveal. BPReveal is free software: You can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. BPReveal is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with BPReveal. If not, see <https://www.gnu.org/licenses/>.  # noqa
